6fb4187e5b7daad60dfac815eed4a3444677295c
Minister124 <kushaltmg321@gmail.com>
2024-06-11 14:14:35 +0545

    low memory usage

--- Easy/Remove Duplicates from Sorted Array/RemoveDuplicatesInArray.cs
+++ Easy/Remove Duplicates from Sorted Array/RemoveDuplicatesInArray.cs
@@ -5,6 +5,8 @@ public class Solution {
         for (int i = 0; i < uniqueNumbers.Length; i++){
             nums[i] = uniqueNumbers[i];
         }
+        //or
+        Array.Copy(uniqueNumbers, nums, uniqueNumbers.Length);
         
         return uniqueNumbers.Length;
     }
